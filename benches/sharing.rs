77bce3f456028f8ea98ff52e83d14146ab5e1174
Morten Dahl <douglas.botley@gmail.com>
2016-11-01 15:14:00 +0100

    ensure that batch sizes match schemes

--- benches/sharing.rs
+++ benches/sharing.rs
@@ -3,9 +3,20 @@ extern crate bencher;
 extern crate threshold_secret_sharing as tss;
 
 use bencher::Bencher;
-use tss::packed::PSS_155_728_100 as pss;
+use tss::packed::*;
 
-pub fn sharing_pack(b: &mut Bencher) {
+pub fn sharing_small(b: &mut Bencher) {
+    let ref pss = PSS_4_26_3;
+    let secrets: Vec<i64> = vec![5 ; 9999];
+    b.iter(|| {
+        let _: Vec<Vec<i64>> = secrets.chunks(pss.secret_count)
+                .map(|batch| pss.share(&batch))
+                .collect();
+    })
+}
+
+pub fn sharing_large(b: &mut Bencher) {
+    let ref pss = PSS_155_728_100;
     let secrets: Vec<i64> = vec![5 ; 10000];
     b.iter(|| {
         let _: Vec<Vec<i64>> = secrets.chunks(pss.secret_count)
@@ -15,7 +26,8 @@ pub fn sharing_pack(b: &mut Bencher) {
 }
 
 benchmark_group!(sharing,
-    sharing_pack
+    sharing_small,
+    sharing_large
 );
 
 benchmark_main!(sharing);
