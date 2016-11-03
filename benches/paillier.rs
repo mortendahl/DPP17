c625e85b39af77d6b6657e13874d3cff32a6e29f
Morten Dahl <douglas.botley@gmail.com>
2016-11-03 18:11:01 +0100

    features

--- benches/paillier.rs
+++ benches/paillier.rs
@@ -1,119 +1,134 @@
+
 #[macro_use]
 extern crate bencher;
+
+#[cfg(feature="inclpaillier")]
 extern crate paillier;
 
-use bencher::Bencher;
-use paillier::*;
+#[cfg(feature="inclpaillier")]
+mod foo {
 
-pub trait TestKeyGeneration
-where
-    Self : PartiallyHomomorphicScheme
-{
-    fn test_keypair() -> (Self::EncryptionKey, Self::DecryptionKey);
-}
+    use bencher::Bencher;
+    use paillier::*;
 
-// 1024 bit primes
-// static P: &'static str = "148677972634832330983979593310074301486537017973460461278300587514468301043894574906886127642530475786889672304776052879927627556769456140664043088700743909632312483413393134504352834240399191134336344285483935856491230340093391784574980688823380828143810804684752914935441384845195613674104960646037368551517";
-// static Q: &'static str = "158741574437007245654463598139927898730476924736461654463975966787719309357536545869203069369466212089132653564188443272208127277664424448947476335413293018778018615899291704693105620242763173357203898195318179150836424196645745308205164116144020613415407736216097185962171301808761138424668335445923774195463";
-
-// 2048 bit primes
-static P: &'static str = "54012895487015803837782421918841304863093162502146915827099238255626761389465957752056702693431430972436786355954646022466841435632094385265559627938436498972714352765471698566168945062965812056432412175521672036039582393637684261505269548649599691053041645072024278713283987472744964393377089048380212183701013564638897218456903964669359622810875460724326972855594957135344351009076932272355015777958742805494234839710255927334289902051693131165245513596331706022111667560809760947628509288759753593140967096047486612859680010875340619186313770693509235798857494768621913543203586903819461926872265770592622637080247";
-static Q: &'static str = "60110804761482905184172241999095064083721568391310132372880785562823040626081548259976195239057024762128798436684644401019565227508680839629752481384744855648596664223620474562582585419094571730852126918991494749938349375651158144545334949768160783962056913632707282062013023732986998195594940491859337992015569093391582644730733764652146222141495874869085082992832080902317418308778550853362446428222413647016439326663338175383509775221151568910938769471308411320393345489705012051577672571014388700476797545130036524629098427518061068575727892423981365405385986469525296662636940291427883820330312960173766723887143";
-
-impl TestKeyGeneration for PackedPaillier {
-    fn test_keypair() -> (<Self as PartiallyHomomorphicScheme>::EncryptionKey, <Self as PartiallyHomomorphicScheme>::DecryptionKey) {
-        let ref p = str::parse(P).unwrap();
-        let ref q = str::parse(Q).unwrap();
-        let ref n = p * q;
-        let plainek = <PlainPaillier as PartiallyHomomorphicScheme>::EncryptionKey::from(n);
-        let plaindk = <PlainPaillier as PartiallyHomomorphicScheme>::DecryptionKey::from(p, q);
-        let ek = <PackedPaillier as PartiallyHomomorphicScheme>::EncryptionKey::from(plainek, 67, 32+29);
-        let dk = <PackedPaillier as PartiallyHomomorphicScheme>::DecryptionKey::from(plaindk, 67, 32+29);
-        (ek, dk)
+    pub trait TestKeyGeneration
+    where
+        Self : PartiallyHomomorphicScheme
+    {
+        fn test_keypair() -> (Self::EncryptionKey, Self::DecryptionKey);
     }
-}
+
+    // 1024 bit primes
+    // static P: &'static str = "148677972634832330983979593310074301486537017973460461278300587514468301043894574906886127642530475786889672304776052879927627556769456140664043088700743909632312483413393134504352834240399191134336344285483935856491230340093391784574980688823380828143810804684752914935441384845195613674104960646037368551517";
+    // static Q: &'static str = "158741574437007245654463598139927898730476924736461654463975966787719309357536545869203069369466212089132653564188443272208127277664424448947476335413293018778018615899291704693105620242763173357203898195318179150836424196645745308205164116144020613415407736216097185962171301808761138424668335445923774195463";
+
+    // 2048 bit primes
+    static P: &'static str = "54012895487015803837782421918841304863093162502146915827099238255626761389465957752056702693431430972436786355954646022466841435632094385265559627938436498972714352765471698566168945062965812056432412175521672036039582393637684261505269548649599691053041645072024278713283987472744964393377089048380212183701013564638897218456903964669359622810875460724326972855594957135344351009076932272355015777958742805494234839710255927334289902051693131165245513596331706022111667560809760947628509288759753593140967096047486612859680010875340619186313770693509235798857494768621913543203586903819461926872265770592622637080247";
+    static Q: &'static str = "60110804761482905184172241999095064083721568391310132372880785562823040626081548259976195239057024762128798436684644401019565227508680839629752481384744855648596664223620474562582585419094571730852126918991494749938349375651158144545334949768160783962056913632707282062013023732986998195594940491859337992015569093391582644730733764652146222141495874869085082992832080902317418308778550853362446428222413647016439326663338175383509775221151568910938769471308411320393345489705012051577672571014388700476797545130036524629098427518061068575727892423981365405385986469525296662636940291427883820330312960173766723887143";
+
+    impl TestKeyGeneration for PackedPaillier {
+        fn test_keypair() -> (<Self as PartiallyHomomorphicScheme>::EncryptionKey, <Self as PartiallyHomomorphicScheme>::DecryptionKey) {
+            let ref p = str::parse(P).unwrap();
+            let ref q = str::parse(Q).unwrap();
+            let ref n = p * q;
+            let plainek = <PlainPaillier as PartiallyHomomorphicScheme>::EncryptionKey::from(n);
+            let plaindk = <PlainPaillier as PartiallyHomomorphicScheme>::DecryptionKey::from(p, q);
+            let ek = <PackedPaillier as PartiallyHomomorphicScheme>::EncryptionKey::from(plainek, 67, 32+29);
+            let dk = <PackedPaillier as PartiallyHomomorphicScheme>::DecryptionKey::from(plaindk, 67, 32+29);
+            (ek, dk)
+        }
+    }
 
 
-pub fn paillier_encryption<PHE>(b: &mut Bencher)
-where
-    PHE : PartiallyHomomorphicScheme,
-    PHE : TestKeyGeneration,
-    PHE::Plaintext : From<Vec<u64>>
-{
-    let (ek, _) = PHE::test_keypair();
-    let plaintexts: Vec<u64> = vec![125; 67];
+    pub fn paillier_encryption<PHE>(b: &mut Bencher)
+    where
+        PHE : PartiallyHomomorphicScheme,
+        PHE : TestKeyGeneration,
+        PHE::Plaintext : From<Vec<u64>>
+    {
+        let (ek, _) = PHE::test_keypair();
+        let plaintexts: Vec<u64> = vec![125; 67];
 
-    b.iter(|| {
-        let _: Vec<PHE::Ciphertext> = plaintexts.chunks(67)
+        b.iter(|| {
+            let _: Vec<PHE::Ciphertext> = plaintexts.chunks(67)
+                    .map(|batch| {
+                        let m = PHE::Plaintext::from(batch.to_vec());
+                        PHE::encrypt(&ek, &m)
+                    })
+                    .collect();
+        });
+    }
+
+
+    pub fn paillier_decryption<PHE>(b: &mut Bencher)
+    where
+        PHE : PartiallyHomomorphicScheme,
+        PHE : TestKeyGeneration,
+        PHE::Plaintext : From<Vec<u64>>
+    {
+        let (ek, dk) = PHE::test_keypair();
+        let plaintexts: Vec<u64> = vec![125; 67];
+        let ciphertexts: Vec<PHE::Ciphertext> = plaintexts.chunks(67)
                 .map(|batch| {
                     let m = PHE::Plaintext::from(batch.to_vec());
                     PHE::encrypt(&ek, &m)
                 })
                 .collect();
-    });
-}
+
+        b.iter(|| {
+            let _: Vec<PHE::Plaintext> = ciphertexts.iter()
+                    .map(|c| {
+                        PHE::decrypt(&dk, c)
+                    })
+                    .collect();
+        });
+    }
 
 
-pub fn paillier_decryption<PHE>(b: &mut Bencher)
-where
-    PHE : PartiallyHomomorphicScheme,
-    PHE : TestKeyGeneration,
-    PHE::Plaintext : From<Vec<u64>>
-{
-    let (ek, dk) = PHE::test_keypair();
-    let plaintexts: Vec<u64> = vec![125; 67];
-    let ciphertexts: Vec<PHE::Ciphertext> = plaintexts.chunks(67)
-            .map(|batch| {
-                let m = PHE::Plaintext::from(batch.to_vec());
-                PHE::encrypt(&ek, &m)
-            })
-            .collect();
-
-    b.iter(|| {
-        let _: Vec<PHE::Plaintext> = ciphertexts.iter()
-                .map(|c| {
-                    PHE::decrypt(&dk, c)
+    pub fn paillier_addition<PHE>(b: &mut Bencher)
+    where
+        PHE : PartiallyHomomorphicScheme,
+        PHE : TestKeyGeneration,
+        PHE::Plaintext : From<Vec<u64>>
+    {
+        let (ek, _) = PHE::test_keypair();
+        let ref ciphertexts1: Vec<PHE::Ciphertext> = vec![125; 67].chunks(67)
+                .map(|batch| {
+                    let m = PHE::Plaintext::from(batch.to_vec());
+                    PHE::encrypt(&ek, &m)
                 })
                 .collect();
-    });
-}
 
-
-pub fn paillier_addition<PHE>(b: &mut Bencher)
-where
-    PHE : PartiallyHomomorphicScheme,
-    PHE : TestKeyGeneration,
-    PHE::Plaintext : From<Vec<u64>>
-{
-    let (ek, _) = PHE::test_keypair();
-    let ref ciphertexts1: Vec<PHE::Ciphertext> = vec![125; 67].chunks(67)
-            .map(|batch| {
-                let m = PHE::Plaintext::from(batch.to_vec());
-                PHE::encrypt(&ek, &m)
-            })
-            .collect();
-
-    let ref ciphertexts2: Vec<PHE::Ciphertext> = vec![125; 67].chunks(67)
-            .map(|batch| {
-                let m = PHE::Plaintext::from(batch.to_vec());
-                PHE::encrypt(&ek, &m)
-            })
-            .collect();
-
-    b.iter(|| {
-        let _: Vec<PHE::Ciphertext> = ciphertexts1.iter().zip(ciphertexts2)
-                .map(|(c1, c2)| {
-                    PHE::add(&ek, &c1, &c2)
+        let ref ciphertexts2: Vec<PHE::Ciphertext> = vec![125; 67].chunks(67)
+                .map(|batch| {
+                    let m = PHE::Plaintext::from(batch.to_vec());
+                    PHE::encrypt(&ek, &m)
                 })
                 .collect();
-    });
+
+        b.iter(|| {
+            let _: Vec<PHE::Ciphertext> = ciphertexts1.iter().zip(ciphertexts2)
+                    .map(|(c1, c2)| {
+                        PHE::add(&ek, &c1, &c2)
+                    })
+                    .collect();
+        });
+    }
+
 }
 
-
+#[cfg(feature="inclpaillier")]
+use self::foo::*;
+#[cfg(feature="inclpaillier")]
 benchmark_group!(paillier,
-    paillier_encryption<PackedPaillier>,
-    paillier_decryption<PackedPaillier>,
-    paillier_addition<PackedPaillier>
+    paillier_encryption<paillier::PackedPaillier>,
+    paillier_decryption<paillier::PackedPaillier>,
+    paillier_addition<paillier::PackedPaillier>
 );
+
+#[cfg(not(feature="inclpaillier"))]
+pub fn dummy(_: &mut bencher::Bencher) {}
+#[cfg(not(feature="inclpaillier"))]
+benchmark_group!(paillier, dummy);
 
 benchmark_main!(paillier);
--- benches/sodium.rs
+++ benches/sodium.rs
@@ -1,30 +1,48 @@
+
 #[macro_use]
 extern crate bencher;
+
+#[cfg(feature="inclsodium")]
 extern crate sodiumoxide;
 
-use bencher::Bencher;
-use sodiumoxide::crypto;
+#[cfg(feature="inclsodium")]
+mod foo {
 
-pub fn sodium_encryption(b: &mut Bencher) {
-    let secrets: Vec<u8> = vec![125; 8*10000];
-    let (ek, _) = crypto::box_::gen_keypair();
-    b.iter(|| {
-        let _ = crypto::sealedbox::seal(&secrets, &ek);
-    });
+    use bencher::Bencher;
+    use sodiumoxide::crypto;
+
+    pub fn sodium_encryption(b: &mut Bencher) {
+        let secrets: Vec<u8> = vec![125; 8*10000];
+        let (ek, _) = crypto::box_::gen_keypair();
+        b.iter(|| {
+            let _ = crypto::sealedbox::seal(&secrets, &ek);
+        });
+    }
+
+    pub fn sodium_decryption(b: &mut Bencher) {
+        let secrets: Vec<u8> = vec![125; 8*10000];
+        let (ek, dk) = crypto::box_::gen_keypair();
+        let enc = crypto::sealedbox::seal(&secrets, &ek);
+        b.iter(|| {
+            let _ = crypto::sealedbox::open(&enc, &ek, &dk).unwrap();
+        });
+    }
+
+
+
 }
 
-pub fn sodium_decryption(b: &mut Bencher) {
-    let secrets: Vec<u8> = vec![125; 8*10000];
-    let (ek, dk) = crypto::box_::gen_keypair();
-    let enc = crypto::sealedbox::seal(&secrets, &ek);
-    b.iter(|| {
-        let _ = crypto::sealedbox::open(&enc, &ek, &dk).unwrap();
-    });
-}
-
+#[cfg(feature="inclsodium")]
+use self::foo::*;
+#[cfg(feature="inclsodium")]
 benchmark_group!(sodium,
     sodium_encryption,
     sodium_decryption
 );
+
+#[cfg(not(feature="inclsodium"))]
+pub fn dummy(_: &mut bencher::Bencher) {}
+#[cfg(not(feature="inclsodium"))]
+benchmark_group!(sodium, dummy);
 
 benchmark_main!(sodium);
